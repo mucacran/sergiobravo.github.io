a727fc26b52fa9d5762a123dbc5a84b7aec1a554
mucacran <el.mucacran.rasta@hotmail.com>
2019-10-12 02:48:36 -0500

    Mejorando el codigo
    De javascript

--- js/fecha.js
+++ js/fecha.js
@@ -13,6 +13,7 @@ function GetModifiedDate()
     document.getElementById("datemodified").innerHTML = modifiedDate;
 }
 
+
 function UpdateDates()
 {
     GetCurrentYear();
--- lesson4/js/fecha.js
+++ lesson4/js/fecha.js
@@ -13,8 +13,23 @@ function GetModifiedDate()
     document.getElementById("datemodified").innerHTML = modifiedDate;
 }
 
+
+function printMeses()
+{
+    var monthName = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    var daysName = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    
+    var fecha = new Date();
+
+    var fechaPrint = daysName[fecha.getDay()] + ', ' + fecha.getDate() + ' ' + monthName[fecha.getMonth()] + ' ' + fecha.getFullYear();
+    var divFecha = document.getElementById('fecha');
+    divFecha.innerHTML = fechaPrint;
+}
+
 function UpdateDates()
 {
     GetCurrentYear();
-    GetModifiedDate();
+    // GetModifiedDate();
+    printMeses();
 }
+
