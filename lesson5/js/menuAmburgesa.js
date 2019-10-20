8941e7812e898f7290a5f6ed6c4f52de7c46c871
mucacran <el.mucacran.rasta@hotmail.com>
2019-10-20 07:26:56 -0500

    Solucionado el menu

--- lesson5/js/menuAmburgesa.js
+++ lesson5/js/menuAmburgesa.js
@@ -1,3 +1,13 @@
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+
+	var elementClasses = document.getElementsByClassName("navigation")[0].classList;
+	console.log(elementClasses.length)
+
+	for(var i=0; i < elementClasses.length;i++)
+	{
+		console.log(elementClasses[i]);
+	}
+
+	
 }
