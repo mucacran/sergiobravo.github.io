55914943600080feda2848c30c10253a90e719fc
mucacran <el.mucacran.rasta@hotmail.com>
2019-10-20 22:21:30 -0500

    Deber terminado

--- lesson5/js/menuAmburgesa.js
+++ lesson5/js/menuAmburgesa.js
@@ -9,3 +9,17 @@ function toggleMenu() {
 		console.log(elementClasses[i]);
 	}	
 }
+
+
+function promocion()
+{
+	var texto = '<p style="text-align:center;padding:15px 0px;margin:0px;background-color:yellow"> Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion. </p>';
+	var divHtml = document.getElementById('promocion');
+
+	var fecha = new Date();
+	
+	if(fecha.getDay() == 6)
+	{
+		divHtml.innerHTML = texto;
+	}
+}
