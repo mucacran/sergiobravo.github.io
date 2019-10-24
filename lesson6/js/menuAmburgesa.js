b1c94566d2cafa2e47a07d025a80ab61a64d5a3e
mucacran <el.mucacran.rasta@hotmail.com>
2019-10-24 05:03:32 -0500

    me faltan  mas cambios con respecto a la imagen responsive

---
+++ lesson6/js/menuAmburgesa.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+
+	var elementClasses = document.getElementsByClassName("navigation")[0].classList;
+	console.log(elementClasses.length)
+
+	for(var i=0; i < elementClasses.length;i++)
+	{
+		console.log(elementClasses[i]);
+	}	
+}
+
+
+function promocion()
+{
+	var texto = '<p style="text-align:center;padding:15px 0px;margin:0px;background-color:yellow"> Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion. </p>';
+	var divHtml = document.getElementById('promocion');
+
+	var fecha = new Date();
+	
+	if(fecha.getDay() == 6)
+	{
+		divHtml.innerHTML = texto;
+	}
+}
