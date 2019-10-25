e6c2bb168324fde8e43550983df1ae42ad6b784f
mucacran <el.mucacran.rasta@hotmail.com>
2019-10-25 05:45:10 -0500

    avance
    Con el codigo de temperatura

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,21 @@
+/*
+The formula to calculate the wind chill factor is LaTeX:
+f=35.74+0.6215\:t-35.75\:s^{0.16}+0.4275\:t\:s^{0.16}
+f = 35.74 + 0.6215 t − 35.75 s 0.16 + 0.4275 t s 0.16 ,
+
+where:
+f is the wind chill factor in Fahrenheit,
+t is the air average temperature in Fahrenheit, and
+s is the wind speed in miles per hour.
+
+Input requirements: "Wind Chill Temperature is officially defined for
+temperatures at or below 10 °C (50 °F) and
+wind speeds above 4.8 kilometres per hour (3.0 mph)."
+*/
+
+var temperatura = 0;
+var velocidadViento = 0;
+function windChill()
+{
+
+}
