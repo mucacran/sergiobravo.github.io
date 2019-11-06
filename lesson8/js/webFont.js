144f7c8aab22951fdebaef94c10cb04b6b88289b
mucacran <el.mucacran.rasta@hotmail.com>
2019-11-06 05:05:20 -0500

    listo termine la tabla

---
+++ lesson8/js/webFont.js
@@ -0,0 +1,14 @@
+// var WebFont = require('webfontloader');
+
+WebFont.load({
+    google: {
+        families: [
+            'Didact Gothic',
+            'Raleway:900'
+        ]
+    }
+});
+
+WebFontConfig = {
+    events: false
+  };
