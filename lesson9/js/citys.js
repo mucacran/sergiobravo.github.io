2362fe568f15022abad998aff87bde9faeedb31d
mucacran <el.mucacran.rasta@hotmail.com>
2019-11-16 16:38:22 -0500

    añadir mas botones al index

--- lesson9/js/citys.js
+++ lesson9/js/citys.js
@@ -6,20 +6,18 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         console.table(jsonObject);  // temporary checking for valid response and data parsing
-    
-
 
     const ciudades = jsonObject['towns'];
     var contenido = '';
     for(var i = 0; i < 3; i++)
     {
-        contenido += '<section><div><h2>' + ciudades[i].name +
-        '</h2><span>' + ciudades[i].motto +
-        '</span><p>Year Found: '+ ciudades[i].yearFounded + 
-        '<br>Population: ' + ciudades[i].currentPopulation +
-        '<br>averageRainfall: ' + ciudades[i].averageRainfall +
-        '</p></div><img src="img/' + ciudades[i].photo + '" alt ="iamgen of ' + ciudades[i].name + '" >' +
-        '</section>';
+        contenido +=    '<section><div><h2>' + ciudades[i].name +
+                        '</h2><span>' + ciudades[i].motto +
+                        '</span><p><b>Year Found:</b> '+ ciudades[i].yearFounded + 
+                        '<br><b>Population:</b> ' + ciudades[i].currentPopulation +
+                        '<br><b>averageRainfall:</b> ' + ciudades[i].averageRainfall +
+                        '</p></div><img src="img/' + ciudades[i].photo + '" alt ="iamgen of ' + ciudades[i].name + '" >' +
+                        '</section>';
     }
     document.getElementById('cards').innerHTML =  contenido;
 });
