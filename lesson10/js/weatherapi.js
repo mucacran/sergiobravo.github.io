f86d037ad7608465ec5fe469f2ad3d41c9dd4579
mucacran <el.mucacran.rasta@hotmail.com>
2019-11-20 07:37:02 -0500

    usando la api del clima de un software

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,22 @@
+var server = 'api.openweathermap.org';
+var api = '5cd682059cff3541db92f27dcde7121d';
+var prestonIdaho = '5604473';
+var city = ''
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${prestonIdaho}&appid=${api}`;
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.table(jsObject);
+
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    /****************************************/
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
+
+  console.log(apiURL);
+  
