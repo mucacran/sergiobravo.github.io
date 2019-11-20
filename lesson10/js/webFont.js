3ab8839508faedbebf81052ad90a9b265753ca3b
mucacran <el.mucacran.rasta@hotmail.com>
2019-11-20 06:48:43 -0500

    pilas

---
+++ lesson10/js/webFont.js
@@ -0,0 +1,17 @@
+
+WebFont.load({
+    google: {
+        families: [
+            'Didact Gothic',
+            'Raleway:900'
+        ]
+    }
+});
+
+WebFontConfig = {
+    events: false
+  };
+
+
+// var WebFont = require('webfontloader');
+
