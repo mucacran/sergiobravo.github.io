f8d237111a4b1ecfaf1ce459126a108620e5ba7f
mucacran <el.mucacran.rasta@hotmail.com>
2019-12-20 19:07:07 -0500

    subiendo imagenes de templos

--- pruebaFinal1/js/templos.js
+++ pruebaFinal1/js/templos.js
@@ -1,5 +1,6 @@
 var temploID = document.getElementById('temple');
 var temploMOstrar = temploID.getAttribute('data-templo');
+var numData = parseInt(temploMOstrar);
 
 const apiURL = "https://mucacran.github.io/sergiobravo.github.io/pruebaFinal1/js/templos.json";
 
@@ -14,7 +15,7 @@ fetch(apiURL)
 
         for (let i = 0; i < thetemplos.length; i++ )
         {
-            if(i == 0)
+            if(i == numData)
             {
                 temploID.innerHTML =    '<img src="' + thetemplos[i].imageurl + '">' + 
                                         '<p> <b>Address</b>: ' + thetemplos[i].address + '</p>' +
@@ -24,19 +25,49 @@ fetch(apiURL)
                                         '<p> <b>History</b>: ' + thetemplos[i].historia + '</p>' +
                                         '<p> <b>Ordinance Schedule</b>: ' + thetemplos[i].calendarioOrdenanza + '</p>' +
                                         '<p> <b>Session Schedule</b>: ' + thetemplos[i].calendarioSecion + '</p>' +
-                                        '<p> <b>Temple Closure Schedule</b>: ' + thetemplos[i].calendarioSierre[0].Wednesday + '</p>';
+                                        '<p> <b>Temple Closure Schedule</b>: Tuesday ' + thetemplos[i].calendarioSierre[0].Tuesday +
+                                        '<br>Tuesday ' + thetemplos[i].calendarioSierre[0].Wednesday + '</p>';
 
             }
-            else if(false)
+            else if(i == numData)
             {
-                console.log('pilas');
+                temploID.innerHTML =    '<img src="' + thetemplos[i].imageurl + '">' + 
+                                        '<p> <b>Address</b>: ' + thetemplos[i].address + '</p>' +
+                                        '<p> <b>Telephone</b>: ' + thetemplos[i].Telephone + '</p>' +
+                                        '<p> <b>Mail</b>: ' + thetemplos[i].CorreoElectrico + '</p>' +
+                                        '<p> <b>Services</b>: ' + thetemplos[i].LosServicios + '</p>' +
+                                        '<p> <b>History</b>: ' + thetemplos[i].historia + '</p>' +
+                                        '<p> <b>Ordinance Schedule</b>: ' + thetemplos[i].calendarioOrdenanza + '</p>' +
+                                        '<p> <b>Session Schedule</b>: ' + thetemplos[i].calendarioSecion + '</p>' +
+                                        '<p> <b>Temple Closure Schedule</b>: Tuesday ' + thetemplos[i].calendarioSierre[0].Tuesday +
+                                        '<br>Tuesday ' + thetemplos[i].calendarioSierre[0].Wednesday + '</p>';
+
             }
-            else if(false)
+            else if(i == numData)
             {
-                console.log('pilas');
+                temploID.innerHTML =    '<img src="' + thetemplos[i].imageurl + '">' + 
+                                        '<p> <b>Address</b>: ' + thetemplos[i].address + '</p>' +
+                                        '<p> <b>Telephone</b>: ' + thetemplos[i].Telephone + '</p>' +
+                                        '<p> <b>Mail</b>: ' + thetemplos[i].CorreoElectrico + '</p>' +
+                                        '<p> <b>Services</b>: ' + thetemplos[i].LosServicios + '</p>' +
+                                        '<p> <b>History</b>: ' + thetemplos[i].historia + '</p>' +
+                                        '<p> <b>Ordinance Schedule</b>: ' + thetemplos[i].calendarioOrdenanza + '</p>' +
+                                        '<p> <b>Session Schedule</b>: ' + thetemplos[i].calendarioSecion + '</p>' +
+                                        '<p> <b>Temple Closure Schedule</b>: Tuesday ' + thetemplos[i].calendarioSierre[0].Tuesday +
+                                        '<br>Tuesday ' + thetemplos[i].calendarioSierre[0].Wednesday + '</p>';
+
             }
             else{
-                console.log('pilas');
+                temploID.innerHTML =    '<img src="' + thetemplos[i].imageurl + '">' + 
+                                        '<p> <b>Address</b>: ' + thetemplos[i].address + '</p>' +
+                                        '<p> <b>Telephone</b>: ' + thetemplos[i].Telephone + '</p>' +
+                                        '<p> <b>Mail</b>: ' + thetemplos[i].CorreoElectrico + '</p>' +
+                                        '<p> <b>Services</b>: ' + thetemplos[i].LosServicios + '</p>' +
+                                        '<p> <b>History</b>: ' + thetemplos[i].historia + '</p>' +
+                                        '<p> <b>Ordinance Schedule</b>: ' + thetemplos[i].calendarioOrdenanza + '</p>' +
+                                        '<p> <b>Session Schedule</b>: ' + thetemplos[i].calendarioSecion + '</p>' +
+                                        '<p> <b>Temple Closure Schedule</b>: Tuesday ' + thetemplos[i].calendarioSierre[0].Tuesday +
+                                        '<br>Tuesday ' + thetemplos[i].calendarioSierre[0].Wednesday + '</p>';
             }
         }
     }
